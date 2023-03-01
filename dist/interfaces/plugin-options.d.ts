def6274b9c5efe2dde65c29e52111f569338b6f6
William Moraes <will.moraes.96@gmail.com>
2023-03-01 10:47:36 -0300

    Readme for defaultKeyPrefix

---
+++ dist/interfaces/plugin-options.d.ts
@@ -0,0 +1,7 @@
+import { OptionsInterface } from './options';
+/**
+ * The Interface that is responsible for the Options provided.
+ */
+export interface PluginOptionsInterface extends OptionsInterface {
+    shared?: boolean;
+}
